217924983cd9254c24be9db17f905ba515a10512
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-04-21 00:02:22 +0300

    Добавление сервиса для транспортных средств

---
+++ lib/Services/bookingServices.dart
@@ -0,0 +1,350 @@
+import 'package:metion/Logic/BookingLogic/bookingCollectionData.dart';
+import 'package:metion/Logic/sharedPreference.dart';
+import 'package:metion/Models/driversModel.dart';
+import 'package:metion/Services/apiConstants.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as https;
+import 'package:metion/Services/apiStatus.dart';
+import 'package:http/http.dart';
+
+class BookingServices{
+  static dynamic apiUrl = APIConstants.baseUrl;
+  static dynamic result = BookingCollections.bookingDetails[0].item;
+  static Future<Object> bookingCost(itemNumber, itemSize,distance) async {
+    try {
+      var body = jsonEncode({
+        "itemNumber":itemNumber,
+        "itemSize":itemSize,
+        "distance":distance
+      });
+      var url = Uri.parse("$apiUrl/booking/calculate-booking-cost");
+      String token = await UserPreferences().getToken();
+      Response response = await https.post(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+  }
+
+  static Future<Object> matchADriver(String fullName, String phoneNumber) async {
+    try {
+      var paymentType = await UserPreferences().getPaymentType();
+
+      var results;
+      results = BookingCollections.bookingDetails[0].item;
+      var items = {
+        "size":results["itemSize"].toString(),
+        "number":results["itemCount"],
+        "name":results["itemName"].toString(),
+        "weight":result["weight"]
+      };
+      var body = jsonEncode({
+      "latitude": BookingCollections.bookingDetails[0].sourceLatitude,
+      "longitude": BookingCollections.bookingDetails[0].sourceLogitude,
+      "amount": BookingCollections.bookingDetails[0].amount,
+      "customerName": fullName,
+      "sourceAddress": BookingCollections.bookingDetails[0].sourceAddress,
+      "destinationAddress": BookingCollections.bookingDetails[0].destinationAddress,
+      "phoneNumber": phoneNumber,
+      "item": items,
+      "paymentMethod":  paymentType.toLowerCase(),
+
+      });
+
+      var url = Uri.parse("$apiUrl/drivers/match-driver");
+      String token = await UserPreferences().getToken();
+      Response response = await https.post(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+
+      if (jsonDecoded['status'] == true) {
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+
+
+    }
+
+}
+
+
+  static Future<Object> createBookingCollection(Object driverInfo, Object companyDetails, String driverId, String driverAuthId) async {
+    try {
+      var paymentType = await UserPreferences().getPaymentType();
+      var items = {
+        "size":result["itemSize"].toString(),
+        "number":result["itemCount"],
+        "name":result["itemName"].toString(),
+        "weight":result["weight"]
+
+      };
+      // var receiver = {
+      //   "name" : "Prince John",
+      //   "phoneNumber":"+23498454543",
+      //   "address":"Orlu Rd Amakaohia Akwakuma",
+      //   "gender":"Male"
+      // };
+      var body = jsonEncode({
+        "sourceLatitude":BookingCollections.bookingDetails[0].sourceLatitude,
+        "sourceLogitude":BookingCollections.bookingDetails[0].sourceLogitude,
+        "destinationLatitude":BookingCollections.bookingDetails[0].destinationLatitude,
+        "destinationLogitude":BookingCollections.bookingDetails[0].destinationLogitude,
+        "item":items,
+        "receiver":BookingCollections.bookingDetails[0].receiver,
+        "sender":BookingCollections.bookingDetails[0].sender,
+        "driverInfo":driverInfo,
+        "amount":BookingCollections.bookingDetails[0].amount,
+        "sourceAddress":BookingCollections.bookingDetails[0].sourceAddress,
+        "destinationAddress":BookingCollections.bookingDetails[0].destinationAddress,
+        "totalAmount":BookingCollections.bookingDetails[0].totalAmount,
+        "companyDetails":companyDetails,
+        "distance":BookingCollections.bookingDetails[0].distance,
+        "timeTaken":BookingCollections.bookingDetails[0].timeTaken,
+        "country":BookingCollections.bookingDetails[0].country,
+        "state":BookingCollections.bookingDetails[0].state,
+        "methodOfPayment":paymentType.toString().toLowerCase(),
+        "driverId":driverId,
+        "driverAuthId":driverAuthId,
+      });
+      var url = Uri.parse("$apiUrl/booking/booking-account");
+      String token = await UserPreferences().getToken();
+      Response response = await https.post(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+
+
+        return Success(response: response,data: jsonDecoded);
+      }
+
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+
+  }
+
+
+  static Future<Object> driverConnectionStatus(String driverId) async {
+    try {
+      var url = Uri.parse("$apiUrl/drivers/get-driver-booking-connection?driverId=$driverId&type=accept");
+      String token = await UserPreferences().getToken();
+      Response response = await https.get(url, headers: {'Content-Type': 'application/json','authorization':token });
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+
+  }
+
+
+  static Future<Object> updateBooking( item,isLegal,bookingId,totalAmount, amount) async {
+    try {
+
+      var body = jsonEncode({
+        "item":item,
+        "isLegal":isLegal,
+        "bookingId":bookingId,
+        "totalAmount":totalAmount,
+        "amount":amount
+      });
+      var url = Uri.parse("$apiUrl/booking/update-booking");
+      String token = await UserPreferences().getToken();
+      Response response = await https.put(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+
+
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+
+  }
+
+  static Future<Object> cancelBooking( message) async {
+    try {
+
+      var body = jsonEncode({
+        "message":message,
+      });
+      var url = Uri.parse("$apiUrl/booking/cancel-booking");
+      String token = await UserPreferences().getToken();
+      Response response = await https.post(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+
+
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+
+  }
+
+  static Future<Object> getABooking(customerAuthId) async {
+    try {
+      var url = Uri.parse("$apiUrl/booking/get-a-booking?customerAuthId=$customerAuthId");
+      String token = await UserPreferences().getToken();
+      Response response = await https.get(url, headers: {'Content-Type': 'application/json','authorization':token });
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+  }
+
+
+  static Future<Object> customerConfirmBooking( bookingId,customerAuth) async {
+    try {
+
+      var body = jsonEncode({
+        "bookingId":bookingId,
+        "customerAuth":customerAuth
+      });
+      var url = Uri.parse("$apiUrl/booking/customer-confirm-booking");
+      String token = await UserPreferences().getToken();
+      Response response = await https.post(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+
+
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+
+  }
+
+  static Future<Object> getBookingRequirements() async {
+    try {
+      String token = await UserPreferences().getToken();
+      var url = Uri.parse("$apiUrl/commons/get-commons?page=1");
+
+      Response response = await https.get(url, headers: {'Content-Type': 'application/json','authorization':token });
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+
+      if (jsonDecoded['status'] == true) {
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+  }
+}
+
+
+
+
---
+++ lib/Services/driverService.dart
@@ -0,0 +1,326 @@
+import 'package:metion/Logic/BookingLogic/bookingCollectionData.dart';
+import 'package:metion/Logic/sharedPreference.dart';
+import 'package:metion/Models/driversModel.dart';
+import 'package:metion/Services/apiConstants.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as https;
+import 'package:metion/Services/apiStatus.dart';
+import 'package:http/http.dart';
+
+class DriverServices{
+  static dynamic apiUrl = APIConstants.baseUrl;
+  static Future<Object> getADriver(driverId) async {
+    try {
+      var url = Uri.parse("$apiUrl/drivers/get-a-driver?driverAuthId=$driverId");
+      String token = await UserPreferences().getToken();
+      Response response = await https.get(url, headers: {'Content-Type': 'application/json','authorization':token });
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+  }
+
+  static Future<Object> updateDriverStatus(driverId,type) async {
+    var body = jsonEncode({
+      "driverId":driverId,
+      "type":type
+    });
+    try {
+      var url = Uri.parse("$apiUrl/drivers/driver-status");
+      String token = await UserPreferences().getToken();
+      Response response = await https.put(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+  }
+
+  static Future<Object> updateDriverLocation(driverId,latitude,logitude,currentLocationAddress) async {
+    var body = jsonEncode({
+      "driverId":driverId,
+      "latitude":5.5134,//latitude,
+      "logitude":7.1629,//logitude,
+      "currentLocationAddress":currentLocationAddress
+    });
+    try {
+      var url = Uri.parse("$apiUrl/drivers/update-current-location");
+      String token = await UserPreferences().getToken();
+      Response response = await https.put(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+
+      //print(response.body);
+      if (jsonDecoded['status'] == true) {
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+  }
+
+
+  static Future<Object> getADriverBookingConnection(driverId) async {
+          try {
+          var url = Uri.parse("$apiUrl/drivers/get-driver-booking-connection?driverId=$driverId&type=connection");
+          String token = await UserPreferences().getToken();
+          Response response = await https.get(url, headers: {'Content-Type': 'application/json','authorization':token });
+          final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+          if (jsonDecoded['status'] == true) {
+          return Success(response: response,data: jsonDecoded);
+          }
+          return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+          } on HttpException {
+          return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+          } on FormatException {
+          return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+          } on SocketException {
+          return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+          } on TimeoutException{
+          return Failure(code: TIME_OUT, errorResponse: "Time out error");
+          }
+
+          catch (e) {
+          return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+          }
+}
+
+  static Future<Object> getADriverBookingDecision(driverId,customerId,type) async {
+
+    var body = jsonEncode({
+      "driverId":driverId,
+      "customerId":customerId,
+      "type":type
+    });
+    try {
+      var url = Uri.parse("$apiUrl/drivers/driver-booking-decision");
+      String token = await UserPreferences().getToken();
+      Response response = await https.put(url, headers: {'Content-Type': 'application/json','authorization':token },body:body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+  }
+
+  static Future<Object> driverConfirmBooking(driverId,companyId,bookingAmount,distance) async {
+
+    var body = jsonEncode({
+      "driverId":driverId,
+      "companyId":companyId,
+      "bookingAmount":bookingAmount,
+      "distance":distance,
+    });
+    try {
+      var url = Uri.parse("$apiUrl/booking/driver-confirm-booking");
+      String token = await UserPreferences().getToken();
+      Response response = await https.post(url, headers: {'Content-Type': 'application/json','authorization':token },body:body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+  }
+
+  static Future<Object>registerDriver(
+      companyId,
+      homeAddress,
+      currentLocation,
+      currentLocationLat,
+      currentLocationLog,
+      lincense,
+      owner,
+      country,
+      state,
+      driverEmail
+      ) async {
+    try {
+
+      var body = jsonEncode({
+        "companyId":companyId,
+        "homeAddress":homeAddress,
+        "currentLocation":currentLocation,
+        "currentLocationLat":currentLocationLat,
+        "currentLocationLog":-122.406417,//currentLocationLog,
+        "lincense":lincense,
+        "owner":owner,
+        "country":country,
+        "state":state,
+        "driverEmail":driverEmail,
+      });
+      var url = Uri.parse("$apiUrl/drivers/create-driver-account");
+      String token = await UserPreferences().getToken();
+      Response response = await https.post(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+
+
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+     }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+
+
+
+}
+  static Future<Object> driverBookingRejections( driverId,firstName,lastName,email,companyId,customerInfo,phoneNumber,profilePicture,amount,companyInfo,bookingDetails) async {
+    try {
+
+      var body = jsonEncode({
+        "driverId":driverId,
+        "firstName":firstName,
+        "lastName":lastName,
+        "email":email,
+        "companyId":companyId,
+        "customerInfo":customerInfo,
+        "phoneNumber":phoneNumber,
+        "profilePicture":profilePicture,
+        "companyInfo":companyInfo,
+        "bookingDetails":bookingDetails
+
+      });
+      var url = Uri.parse("$apiUrl/drivers/create-rejected-booking");
+      String token = await UserPreferences().getToken();
+      Response response = await https.post(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+
+
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+
+
+  }
+
+  static Future<Object> ratingDriver( message,companyID,rate,customerInfo,driverInfo,driverId) async {
+    try {
+
+      var body = jsonEncode({
+        "message":message,
+        "companyID":companyID,
+        "rate":rate,
+        "customerInfo":customerInfo,
+        "driverInfo":driverInfo,
+        "driverId":driverId
+      });
+      var url = Uri.parse("$apiUrl/drivers/rate-a-driver");
+      String token = await UserPreferences().getToken();
+      Response response = await https.put(url, headers: {'Content-Type': 'application/json','authorization':token },body: body);
+      final Map<String, dynamic> jsonDecoded = json.decode(response.body);
+      if (jsonDecoded['status'] == true) {
+
+
+        return Success(response: response,data: jsonDecoded);
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: jsonDecoded['message']);
+    } on HttpException {
+      return Failure(code: NO_INTERNET, errorResponse: "Internal server error");
+    } on FormatException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "Invalid format");
+    } on SocketException {
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: "No internet connection");
+    } on TimeoutException{
+      return Failure(code: TIME_OUT, errorResponse: "Time out error");
+    }
+
+    catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: e.toString());
+    }
+
+
+
+  }
+
+}
